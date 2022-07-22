385e4b6876bdc637d592719e2d81640fe57a1b99
Salman Ahmed Abbasis <40638940+SalmanAhmed24@users.noreply.github.com>
2022-07-22 15:26:10 +0500

    fonts still not working

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,13 @@
+import Head from 'next/head';
 import Navbar from '../components/navbar/navbar';
 import '../styles/globals.scss';
 function MyApp({ Component, pageProps }) {
 	return (
 		<div>
+			<Head>
+				<link href="http://fonts.cdnfonts.com/css/futura-md-bt" rel="stylesheet" />
+				<link href="http://fonts.cdnfonts.com/css/souvenir" rel="stylesheet" />
+			</Head>
 			<Navbar />
 			<Component {...pageProps} />
 		</div>
