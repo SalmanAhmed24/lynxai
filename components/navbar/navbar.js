642755f75790e3c0594d10ad0cfb1c114f9ce9b4
Salman Ahmed Abbasis <40638940+SalmanAhmed24@users.noreply.github.com>
2022-07-25 16:01:28 +0500

    made home page mobile responsive

--- components/navbar/navbar.js
+++ components/navbar/navbar.js
@@ -1,19 +1,46 @@
+import React, { useState } from 'react';
+import Drawer from '@mui/material/Drawer';
 function Navbar() {
+	const [ drawerFlag, setDrawerFlag ] = useState(false);
+	const drawerHandler = () => {
+		setDrawerFlag(!drawerFlag);
+	};
 	return (
-		<nav className="nav-cus">
-			<div className="logo-wrap">
-				<img src="./images/logo.png" className="logo" />
-			</div>
-			<ul className="links">
-				<li>Home</li>
-				<li>EU Taxonomy</li>
-				<li>About Us</li>
-			</ul>
-			<div className="cat-btn-wrap">
-				<span className="simple-btn">Contact Us</span>
-				<span className="high-btn">Request a demo</span>
-			</div>
-		</nav>
+		<section>
+			<nav className="nav-cus">
+				<div className="logo-wrap">
+					<img src="./images/logo.png" className="logo" />
+				</div>
+				<ul className="links">
+					<li>Home</li>
+					<li>EU Taxonomy</li>
+					<li>About Us</li>
+				</ul>
+				<div className="cat-btn-wrap">
+					<span className="simple-btn">Contact Us</span>
+					<span className="high-btn">Request a demo</span>
+				</div>
+			</nav>
+			<nav className="mobile">
+				<div className="bar-wrap">
+					<img onClick={() => drawerHandler()} className="bar" src="./images/bar.png" />
+				</div>
+				<Drawer anchor={'left'} open={drawerFlag} onClose={() => drawerHandler()}>
+					<div className="logo-inner-wrap">
+						<img src="./images/logo.png" className="logo-inner" />
+					</div>
+					<div className="inner-content">
+						<p>Home</p>
+						<p>EU Taxonomy</p>
+						<p>About Us</p>
+					</div>
+					<div className="cat-btn-inner-wrap">
+						<span className="simple-btn">Contact Us</span>
+						<span className="high-btn">Request a demo</span>
+					</div>
+				</Drawer>
+			</nav>
+		</section>
 	);
 }
 
