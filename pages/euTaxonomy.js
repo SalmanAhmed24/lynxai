f89064d9b3ae9658d859be854081c18f422511c2
Salman Ahmed Abbasis <40638940+SalmanAhmed24@users.noreply.github.com>
2022-08-22 18:06:17 +0500

    added new animations

--- pages/euTaxonomy.js
+++ pages/euTaxonomy.js
@@ -26,8 +26,24 @@ function EuTaxonomy() {
 					</div>
 				</motion.section>
 			</section>
-			<section className="tax-who-con">
-				<section className="tax-who-sec">
+			<section
+				className="tax-who-con"
+				data-aos="fade-in"
+				data-aos-duration="500"
+				data-aos-easing="ease-in-out"
+				data-aos-mirror="true"
+				data-aos-once="false"
+				data-aos-anchor-placement="top-center"
+			>
+				<section
+					className="tax-who-sec"
+					data-aos="fade-right"
+					data-aos-duration="1000"
+					data-aos-easing="ease-in-out"
+					data-aos-mirror="true"
+					data-aos-once="false"
+					data-aos-anchor-placement="top-center"
+				>
 					<h1>What is EU Taxonomy?</h1>
 					<p>
 						The EU taxonomy is a classification system that establishes a list of environmentally
@@ -38,11 +54,24 @@ function EuTaxonomy() {
 					</p>
 				</section>
 			</section>
-			<section className="tax-sol-con">
+			<section
+				className="tax-sol-con"
+				data-aos="fade-in"
+				data-aos-duration="1000"
+				data-aos-easing="ease-in-out"
+				data-aos-once="false"
+			>
 				<section className="sol-sec">
 					<h1>Our Solutions</h1>
 					<div className="cards-wrap">
-						<div className="sol-cards">
+						<div
+							className="sol-cards"
+							data-aos="fade-left"
+							data-aos-duration="1000"
+							data-aos-easing="ease-in-out"
+							data-aos-once="false"
+							data-aos-anchor-placement="top-center"
+						>
 							<img src="./images/reports.png" className="icon" />
 							<h4>Efficient EU Taxonomy Reporting</h4>
 							<p>
@@ -50,7 +79,15 @@ function EuTaxonomy() {
 								According with the regulator standards.
 							</p>
 						</div>
-						<div className="sol-cards">
+						<div
+							className="sol-cards"
+							data-aos="fade-in"
+							data-aos-delay="500"
+							data-aos-duration="1000"
+							data-aos-easing="ease-in-out"
+							data-aos-once="false"
+							data-aos-anchor-placement="top-center"
+						>
 							<img src="./images/analytics.png" className="icon" />
 							<h4>Control of investments eligibility and alignment</h4>
 							<p>
@@ -58,7 +95,15 @@ function EuTaxonomy() {
 								control any red flags from their investments.
 							</p>
 						</div>
-						<div className="sol-cards">
+						<div
+							className="sol-cards"
+							data-aos="fade-right"
+							data-aos-delay="1000"
+							data-aos-duration="1000"
+							data-aos-easing="ease-in-out"
+							data-aos-once="false"
+							data-aos-anchor-placement="top-center"
+						>
 							<img src="./images/cent.png" className="icon" />
 							<h4>Centralized data from EU Taxonomy</h4>
 							<p>
@@ -66,7 +111,15 @@ function EuTaxonomy() {
 								regarding Taxonomy activities is covered.
 							</p>
 						</div>
-						<div className="sol-cards">
+						<div
+							className="sol-cards"
+							data-aos="fade-in"
+							data-aos-delay="1500"
+							data-aos-duration="1000"
+							data-aos-easing="ease-in-out"
+							data-aos-once="false"
+							data-aos-anchor-placement="top-center"
+						>
 							<img src="./images/realtime.png" className="icon" />
 							<h4>Real-Time Data</h4>
 							<p>
@@ -75,7 +128,15 @@ function EuTaxonomy() {
 								platform.
 							</p>
 						</div>
-						<div className="sol-cards">
+						<div
+							className="sol-cards"
+							data-aos="fade-in"
+							data-aos-delay="2000"
+							data-aos-duration="1000"
+							data-aos-easing="ease-in-out"
+							data-aos-once="false"
+							data-aos-anchor-placement="top-center"
+						>
 							<img src="./images/search.png" className="icon" />
 							<h4>Intelligent Search Service</h4>
 							<p>
@@ -87,11 +148,24 @@ function EuTaxonomy() {
 					</div>
 				</section>
 			</section>
-			<section className="tax-del-con">
+			<section
+				className="tax-del-con"
+				data-aos="fade-in"
+				data-aos-duration="1000"
+				data-aos-easing="ease-in-out"
+				data-aos-once="false"
+			>
 				<section className="del-sec">
 					<h1>What we deliver</h1>
 					<div className="cards-wrap">
-						<div className="sol-cards">
+						<div
+							className="sol-cards"
+							data-aos="fade-left"
+							data-aos-duration="1000"
+							data-aos-easing="ease-in-out"
+							data-aos-once="false"
+							data-aos-anchor-placement="top-center"
+						>
 							<img src="./images/cloud.png" className="icon" />
 							<h4>Interactive Cloud Platform</h4>
 							<p>
@@ -100,7 +174,15 @@ function EuTaxonomy() {
 								Taxonomy of investment fund.
 							</p>
 						</div>
-						<div className="sol-cards">
+						<div
+							className="sol-cards"
+							data-aos="fade-in"
+							data-aos-delay="500"
+							data-aos-duration="1000"
+							data-aos-easing="ease-in-out"
+							data-aos-once="false"
+							data-aos-anchor-placement="top-center"
+						>
 							<img src="./images/analytics.png" className="icon" />
 							<h4>Intelligent Eligibility Statistics</h4>
 							<p>
@@ -108,7 +190,15 @@ function EuTaxonomy() {
 								in2022. According with the regulator standards.
 							</p>
 						</div>
-						<div className="sol-cards">
+						<div
+							className="sol-cards"
+							data-aos="fade-right"
+							data-aos-delay="1000"
+							data-aos-duration="1000"
+							data-aos-easing="ease-in-out"
+							data-aos-once="false"
+							data-aos-anchor-placement="top-center"
+						>
 							<img src="./images/highlight.png" className="icon" />
 							<h4>Highlights EU Taxonomy</h4>
 							<p>
@@ -116,7 +206,15 @@ function EuTaxonomy() {
 								highlights on annual report of companies.
 							</p>
 						</div>
-						<div className="sol-cards">
+						<div
+							className="sol-cards"
+							data-aos="fade-left"
+							data-aos-delay="1500"
+							data-aos-duration="1000"
+							data-aos-easing="ease-in-out"
+							data-aos-once="false"
+							data-aos-anchor-placement="top-center"
+						>
 							<img src="./images/green.png" className="icon" />
 							<h4>Eligibility Green Asset Ratio</h4>
 							<p>
@@ -125,7 +223,15 @@ function EuTaxonomy() {
 								Taxonomy of investment fund.
 							</p>
 						</div>
-						<div className="sol-cards">
+						<div
+							className="sol-cards"
+							data-aos="fade-up"
+							data-aos-delay="2000"
+							data-aos-duration="1000"
+							data-aos-easing="ease-in-out"
+							data-aos-once="false"
+							data-aos-anchor-placement="top-center"
+						>
 							<img src="./images/search.png" className="icon" />
 							<h4>Intelligent Search Service</h4>
 							<p>
@@ -134,7 +240,15 @@ function EuTaxonomy() {
 								looking for and discover the right answers to their questions when they need them.
 							</p>
 						</div>
-						<div className="sol-cards">
+						<div
+							className="sol-cards"
+							data-aos="fade-right"
+							data-aos-delay="2500"
+							data-aos-duration="1000"
+							data-aos-easing="ease-in-out"
+							data-aos-once="false"
+							data-aos-anchor-placement="top-center"
+						>
 							<img src="./images/dash.png" className="icon" />
 							<h4>Dashboards with Eligible Activities</h4>
 							<p>
